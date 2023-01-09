4466699c873e4d4e03a6d8b43f8b6e22332abd55
benites18 <122297466+benites18@users.noreply.github.com>
2023-01-09 11:39:46 -0500

    Add files via upload

---
+++ scripts/enviar.js
@@ -0,0 +1,61 @@
+function error(){
+
+    let Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'error',
+      title: 'Completa correctamente el formulario'
+    })
+  
+  }
+
+  function noerror(){
+
+    let Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Gracias por suscribirse'
+    })
+  
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('suscribirse').value;
+    if(usuario.length < 4 || usuario.value === null || usuario.value === ' ') {
+        error() ;
+      return;
+    }
+    else{
+        noerror();
+        document.getElementById('formulario').reset();
+        return;
+    }
+    
+  }
+
